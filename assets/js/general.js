234cc59f6ff9a71d3f0d300cff1ff8732379dbf6
eTatev <barev.tatevik@gmail.com>
2020-04-13 17:23:00 +0400

    Projects Page is Done! make rigth Menu~

---
+++ assets/js/general.js
@@ -0,0 +1,32 @@
+console.clear();
+jQuery(function ($) {
+    $(document).ready(function () {
+        // ------------------------------------
+        {
+            try {
+                var $dashOffset = $("path").css("stroke-dashoffset");
+
+                $(window).scroll(function () {
+                    var $percentageComplete = (($(window).scrollTop() / ($("html").height() - $(window).height())) * 100);
+                    var $newUnit = parseInt($dashOffset, 10);
+                    var $offsetUnit = $percentageComplete * ($newUnit / 100);
+                    $("path").css("stroke-dashoffset", $newUnit - $offsetUnit);
+                    $("polygon").css("stroke-dashoffset", $newUnit - $offsetUnit);
+                });
+                console.log($('.service_icon_block').offset().top) 
+            } catch(e) {
+                console.log(e);
+            }
+        }
+
+        // home_projects_filter change button
+        {
+            var $filterBtns = $('.home_projects_filter_body > .module-border-wrap');
+            $filterBtns.on('click', function(event) {
+                event.preventDefault();
+                $('.home_projects_filter_body > .module-border-wrap').removeClass('active');
+                $(this).addClass('active');
+            });
+        }
+    });
+});
